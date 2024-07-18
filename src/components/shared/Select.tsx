a43e40f7be2663be4c9002f83ff7e6bab3766a13
Ilhomjon <futurewithme1221@gmail.com>
2024-07-18 16:26:47 +0500

    Optimized components for not extra rerendering

--- src/components/shared/Select.tsx
+++ src/components/shared/Select.tsx
@@ -2,7 +2,6 @@ import { Dropdown } from "primereact/dropdown";
 import React from "react";
 
 function Select({options, selected, handleSelect, placeholder}: any) {
-    console.log('rendered select')
     return (
         <div className="card flex justify-content-center">
         <Dropdown value={selected} onChange={(e) => handleSelect(e.value)} options={options} optionLabel="status"
@@ -10,5 +9,5 @@ function Select({options, selected, handleSelect, placeholder}: any) {
     </div>
     )
 }
-const MemoizedComponent = React.memo(Select)
-export default MemoizedComponent;
+
+export default React.memo(Select);
